8e6600ea32fdcbe7f2699bb42d77ba5c55d249c0
Anton Pashkouski <Wolendrang@gmail.com>
2017-03-23 20:38:39 +0300

    First tree tasks were completed.

---
+++ 2.py
@@ -0,0 +1,12 @@
+
+def decrypt_byte_string(byte_string):
+    exception_list = [ord(' '), ord('.'), ord('('), ord(')')]
+    pre_result_string = bytes([c if c in exception_list else c + 2 for c in byte_string]).decode("utf-8")
+    translation_table = str.maketrans("{|", "ab")
+    return pre_result_string.translate(translation_table)
+
+def main():
+    print(decrypt_byte_string(b"map"))
+
+if __name__ == "__main__":
+    main()
---
+++ 3.py
@@ -0,0 +1,24 @@
+from collections import Counter
+
+
+def get_symbols_rate():
+    with open("3.txt") as target_file:
+        counter_obj = Counter(letter for line in target_file
+                                     for letter in line.lower())
+        return counter_obj
+
+def print_symbols_rate(counter_obj):
+    for key in counter_obj:
+        value = counter_obj[key]
+        print("{0}: {1}".format(key, str(value)))
+
+
+def main():
+    counter = get_symbols_rate()
+    print_symbols_rate(counter)
+
+
+if __name__ == "__main__":
+    main()
+
+#answer: equality
